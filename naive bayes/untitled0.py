02e048e4be292689839cbf231721de9936b2bff0
cxjoke <40422538+cxjoke@users.noreply.github.com>
2019-07-27 21:57:54 +0800

    Add files via upload

---
+++ naive bayes/untitled0.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 25 20:31:05 2018
+
+@author: chenxi
+"""
+if __name__ == '__main__':
+    a=open(r'C:\Users\chenxi\Desktop\python.py\朴素贝叶斯\ham\1.txt')
+    print(a.read())
